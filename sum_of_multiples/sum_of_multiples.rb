3e4551d7511fd6bab78a225eb1b25101631c9eff
David Renz <davidrenz60@gmail.com>
2016-04-11 20:57:53 -0700

    refactor sum_of_multiples.rb after live session

--- sum_of_multiples/sum_of_multiples.rb
+++ sum_of_multiples/sum_of_multiples.rb
@@ -1,15 +1,15 @@
 class SumOfMultiples
-  def initialize(*multiples)
-    @multiples = multiples
+  def initialize(*factors)
+    @factors = factors
   end
 
   def to(max_value)
-    @multiples.each_with_object([0]) do |multiple, result|
-      (1...max_value).each { |num| result << num if num % multiple == 0 }
-    end.uniq.reduce(:+)
+    (0...max_value).select do |multiple|
+      @factors.any? { |factor| multiple % factor == 0 }
+    end.reduce(:+)
   end
 
   def self.to(max_value)
-    SumOfMultiples.new(3, 5).to(max_value)
+    new(3, 5).to(max_value)
   end
 end
