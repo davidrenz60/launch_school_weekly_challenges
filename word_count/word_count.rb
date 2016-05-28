35403120faa8b449f9973fc1d69448cd73eaf56f
David Renz <davidrenz60@gmail.com>
2016-05-28 07:39:50 -0700

    change downcase call to before map

--- word_count/word_count.rb
+++ word_count/word_count.rb
@@ -2,8 +2,8 @@ class Phrase
   attr_reader :word_count
 
   def initialize(text)
-    @word_count = text.scan(/[a-zA-Z0-9']+/)
-                  .map { |word| word.downcase.gsub((/^'|'$/), '') }
+    @word_count = text.downcase.scan(/[a-z0-9']+/)
+                  .map { |word| word.gsub((/^'|'$/), '') }
                   .each_with_object(Hash.new(0)) { |word, hash| hash[word] += 1 }
   end
 end
