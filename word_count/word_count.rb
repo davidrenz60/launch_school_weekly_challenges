51fe8ad82e2d299b3bcea3762899a22cabf9f6c7
David Renz <davidrenz60@gmail.com>
2016-05-29 07:00:50 -0700

    refactor regexp

--- word_count/word_count.rb
+++ word_count/word_count.rb
@@ -2,8 +2,7 @@ class Phrase
   attr_reader :word_count
 
   def initialize(text)
-    @word_count = text.downcase.scan(/[a-z0-9']+/)
-                  .map { |word| word.gsub((/^'|'$/), '') }
+    @word_count = text.downcase.scan(/\w+'?\w+|\d+/)
                   .each_with_object(Hash.new(0)) { |word, hash| hash[word] += 1 }
   end
 end
