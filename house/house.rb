b1ce4feafe29111b09bfacaab4eaa6e9bd47a210
David Renz <davidrenz60@gmail.com>
2016-06-05 10:08:53 -0700

    refactor house based on live session

--- house/house.rb
+++ house/house.rb
@@ -1,29 +1,19 @@
 class House
   def self.recite
-    House.new.make_poem
+    new.make_poem
   end
 
   def make_poem
-    parts = pieces.reverse
-    rhyme = ""
-    (1..parts.size).each do |index|
-      rhyme << "This is "
-      parts.take(index).reverse_each do |line|
-        rhyme << make_line(line)
-      end
-    end
-
-    rhyme.chomp
+    (1..pieces.size)
+      .map { |piece| make_verse(pieces.last(piece)) }
+      .join("\n\n") + "\n"
   end
 
   private
 
-  def make_line(line)
-    if line[1]
-      line[0] + "\n" + line[1] + " "
-    else
-      line[0] + ".\n\n"
-    end
+  def make_verse(pieces)
+    verse = pieces.map { |piece| piece.join("\n") }.join(" ")
+    "This is #{verse}."
   end
 
   def pieces
