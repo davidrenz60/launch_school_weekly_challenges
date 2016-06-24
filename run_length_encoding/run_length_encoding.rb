94325bc943e2dda63ecc81c1b41e9ef4cff25502
David Renz <davidrenz60@gmail.com>
2016-06-24 16:12:59 -0700

    refactor run length encoding challenge

--- run_length_encoding/run_length_encoding.rb
+++ run_length_encoding/run_length_encoding.rb
@@ -1,35 +1,14 @@
 class RunLengthEncoding
   def self.encode(input)
-    i = 0
-    count = 1
-    output = ''
-    while i < input.length
-      if input[i] == input[i + 1]
-        count += 1
-      else
-        output += count == 1 ? input[i] : "#{count}#{input[i]}"
-        count = 1
-      end
-      i += 1
-    end
-    output
+    input.chars.chunk { |char| char }
+         .map do |char, group|
+           group.size == 1 ? char : "#{group.size}#{char}"
+         end.join
   end
 
   def self.decode(input)
-    i = 0
-    count = ''
-    output = ''
-    while i < input.length
-      if /\d/ =~ input[i]
-        count += input[i]
-      else
-        count = count.to_i
-        count = 1 if count == 0
-        output += input[i] * count
-        count = ''
-      end
-      i += 1
-    end
-    output
+    input.scan(/\d+.|./).map do |group|
+      group.size == 1 ? group : group[-1] * group.to_i
+    end.join
   end
 end
