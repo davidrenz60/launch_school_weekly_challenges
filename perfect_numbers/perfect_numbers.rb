708144ade03b0e21b0fec31d52182b58c17de391
David Renz <davidrenz60@gmail.com>
2016-07-01 10:38:05 -0700

    add perfect numbers challenge

---
+++ perfect_numbers/perfect_numbers.rb
@@ -0,0 +1,20 @@
+module PerfectNumber
+  def self.classify(number)
+    raise "number must be greater than 0" unless number > 0
+
+    sum = aliquot_sum(number)
+    if sum == number
+      "perfect"
+    elsif sum < number
+      "deficient"
+    else
+      "abundant"
+    end
+  end
+
+  def self.aliquot_sum(number)
+    (1..number / 2).each_with_object([0]) do |divisor, factors|
+      factors << divisor if number % divisor == 0
+    end.reduce(:+)
+  end
+end
---
+++ perfect_numbers/perfect_numbers_test.rb
@@ -0,0 +1,32 @@
+require 'minitest/autorun'
+require_relative 'perfect_numbers'
+
+class PerfectNumberTest < Minitest::Test
+  def test_initialize_perfect_number
+    assert_raises RuntimeError do
+      PerfectNumber.classify(-1)
+    end
+  end
+
+  def test_zero_raises_error
+    assert_raises RuntimeError do
+      PerfectNumber.classify(0)
+    end
+  end
+
+  def test_one_is_deficient
+    assert_equal 'deficient', PerfectNumber.classify(1)
+  end
+
+  def test_classify_deficient
+    assert_equal 'deficient', PerfectNumber.classify(13)
+  end
+
+  def test_classify_perfect
+    assert_equal 'perfect', PerfectNumber.classify(28)
+  end
+
+  def test_classify_abundant
+    assert_equal 'abundant', PerfectNumber.classify(12)
+  end
+end
