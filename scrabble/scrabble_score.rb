0f8d8f3a1a6b1780b1cd9b53e4b6a90ffa17fe61
David Renz <davidrenz60@gmail.com>
2016-08-05 06:13:41 -1000

    add scrabble challenge

---
+++ scrabble/scrabble_score.rb
@@ -0,0 +1,20 @@
+class Scrabble
+  SCORES = { "a" => 1, "e" => 1, "i" => 1, "o" => 1, "u" => 1, "l" => 1,
+             "n" => 1, "r" => 1, "s" => 1, "t" => 1, "d" => 2, "g" => 2,
+             "b" => 3, "c" => 3, "m" => 3, "p" => 3, "f" => 4, "h" => 4,
+             "v" => 4, "w" => 4, "y" => 4, "k" => 5, "j" => 8, "x" => 8,
+             "q" => 10, "z" => 10 }.freeze
+
+  def initialize(word)
+    word ||= ""
+    @word = word.downcase.strip
+  end
+
+  def score
+    @word.chars.reduce(0) { |sum, letter| sum + SCORES[letter] }
+  end
+
+  def self.score(word)
+    new(word).score
+  end
+end
